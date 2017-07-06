feb27e24e2ead1e3072e4c47292abe727bb03dca
steven <steven@peipeiyun.com>
2017-07-06 13:50:59 +0800

    完成点击事件
    注册样式点击完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,20 @@
 var app = getApp()
 Page({
   data: {
-    datalist: ["汽配商", "修理厂", "4S店","其他"],
+    datalist:[{
+      name: "汽配商",
+      clickid:1,
+    },{
+        name: "修理厂",
+        clickid: 2,
+    },{
+        name: "4S店",
+        clickid: 3,
+    },{
+        name: "其他",
+        clickid: 4,
+    }],
+    userclickid:0,
     userInfo: {}
   },
   //事件处理函数
@@ -11,6 +24,41 @@ Page({
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  typeClick:function(e){
+    console.log(`点击了${e.target.dataset.idx}`)
+    this.setData({
+      userclickid: e.target.dataset.idx
+    })
+  },
+  formSubmit:function(e){
+    var inputphone = e.detail.value.inputphone;
+    var inputname = e.detail.value.inputname;
+    var inputfactory = e.detail.value.inputfactory;
+    var inputcity = e.detail.value.inputcity;
+    if (inputphone.length==0){
+      wx.navigateTo({
+        url: '../topay/index'
+      })
+      // wx.showToast({
+
+      //   title: '手机号码或密码不得为空!',
+
+      //   icon: 'loading',
+
+      //   duration: 1500
+
+      // })
+      // setTimeout(function () {
+
+      //   wx.hideToast()
+
+      // }, 2000)
+    }else{
+      wx.navigateTo({
+        url: '../logs/logs'
+      })
+    }
   },
   onLoad: function () {
     console.log('onLoad')
